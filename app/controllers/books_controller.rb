b2c17afd7aa59bdc01338b9a39ff5d261d2ddc4d
dante <dantecherng@hotmail.com.tw>
2020-11-25 12:37:31 +0800

    add create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,34 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
 
-def index
- @books = Book.all
-end
+  def new
+    @book = Book.new
+  end
 
-def new
- @book = Book.new
-end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path, notice: "新增成功囉"
+    else
+      render new_book_path
+    end
+  end
 
-def create
-  
-end
+  def show
+  end
 
-def show
-end
+  def edit
+  end
 
+  def update
+  end
 
-def edit
-  
-end
+  def destory
+  end
 
-def update
-  
-end
-
-def destory
-  
-end
-
-
-
+  def book_params
+    params.require(:book).permit(:title)
+  end
 end
