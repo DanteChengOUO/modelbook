ddfab3df164bdf8dcf572ab598c9457670185d85
dante <dantecherng@hotmail.com.tw>
2020-11-25 11:39:52 +0800

    add index

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+
+def index
+ 
+end
+
+def new
+ @book = Book.new
+end
+
+def create
+  
+end
+
+def show
+end
+
+
+def edit
+  
+end
+
+def update
+  
+end
+
+def destory
+  
+end
+
+
+
+end
